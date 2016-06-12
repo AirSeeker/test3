44fb4bbe775bc8d9b6492182ccf354ec33d93d39
AirSeeker <zimorget@gmail.com>
2016-06-12 08:12:34 +0300

    v0.0.2

--- js/script.js
+++ js/script.js
@@ -7,8 +7,27 @@ function getRandomColor() {
     return color;
 }
 
-function setColorForRandomTwoBoxes() {
-  var listOfRandomNumbers =[0,1,2,3].sort(function(){return .5 - Math.random()});
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function setColorForRandomTwoBoxes(gridarr) {
+  var listOfRandomNumbers = shuffle(gridarr);
   while(listOfRandomNumbers.length > 0){
   var cuted = listOfRandomNumbers.splice(0, 2);
   var color = getRandomColor();
@@ -18,28 +37,48 @@ function setColorForRandomTwoBoxes() {
   $('.row').find(PairTwo).css('background-color', color);
   }
 }
-setColorForRandomTwoBoxes();
 
+var gridLinks = ['grid/2x2.html', 'grid/3x3.html', 'grid/4x4.html', 'grid/5x5.html', 'grid/6x6.html'];
+var gridArrays = ['0,1,2,3','0,1,2,3,4,5','0,1,2,3,4,5,6,7','0,1,2,3,4,5,6,7,8,9','0,1,2,3,4,5,6,7,8,9,10,11'];
+var gridCounter = 2;
+
+var round = 0;
+var roundTimer = '';
 var pairCheck = [];
 var linkPairCheck = [];
 var clickCounter = 0;
 var winStatus = '';
-$('.card').bind({
-    click: function() {
-      console.log(clickCounter);
+
+
+function setGrid(gridSystem){
+    var jqXHR = $.get(gridSystem);
+  jqXHR.complete(function(response) {
+    var html = response.responseText;
+    $(".container").html(html);
+    var nextGridArr = gridArrays[round].split(',');
+    setColorForRandomTwoBoxes(nextGridArr);
+  });
+}
+
+
+ function onClickEventHandler(card) {
       if(clickCounter < 2){
-        $(this).toggleClass('flipped');
-        $(this).closest('.box').removeClass('hvr-pulse-grow');
-        var color = $(this).children('.back').css('background-color');
+        card.toggleClass('flipped');
+        card.closest('.box').removeClass('hvr-pulse-grow');
+        var color = card.children('.back').css('background-color');
 
         if(pairCheck.length >= 2){
           pairCheck= [];
           linkPairCheck = [];
           clickCounter = 0;
+          winStatus = ''
         }
 
         pairCheck.push(color);
-        linkPairCheck.push('#'+ $(this).attr('id'));
+        linkPairCheck.push('#'+ card.attr('id'));
+
+        $(linkPairCheck[0]).attr('onclick','');
+        $(linkPairCheck[1]).attr('onclick','');
 
 
         if(pairCheck.length > 1){
@@ -49,6 +88,8 @@ $('.card').bind({
               $(linkPairCheck[1]).removeClass('flipped');
               $(linkPairCheck[0]).closest('.box').addClass('hvr-pulse-grow');
               $(linkPairCheck[1]).closest('.box').addClass('hvr-pulse-grow');
+              $(linkPairCheck[0]).attr('onclick','onClickEventHandler($(this))');
+              $(linkPairCheck[1]).attr('onclick','onClickEventHandler($(this))');
               pairCheck = [];
               linkPairCheck = [];
               clickCounter = 0;
@@ -60,13 +101,39 @@ $('.card').bind({
               linkPairCheck = [];
               clickCounter = 0;
               winStatus += 1;
-              if(winStatus.length === 2){
-                alert('You won!');
+              if(winStatus.length === gridCounter){
+                pairCheck= [];
+                linkPairCheck = [];
+                clickCounter = 0;
+                winStatus = ''
+                roundTimer+=1;
+                $('#status span').html(Number($('#status span').html()) -1);
+                setGrid(gridLinks[round]);
+                if(roundTimer.length === 4){
+                  roundTimer='';
+                  round++;
+                  gridCounter++;
+                  if(round > 4){
+                    alert('You Won!');
+                     gridCounter = 2;
+                     round = 0;
+                     roundTimer = '';
+                     pairCheck = [];
+                     linkPairCheck = [];
+                     clickCounter = 0;
+                     winStatus = '';
+                    setGrid(gridLinks[round]);
+                  }else{
+                    alert('Next round!');
+                    setGrid(gridLinks[round]);
+                  }
+                }
               }
             }
           }, 800);
         }
       clickCounter++;
       }
-    }
-});
+  }
+
+setGrid(gridLinks[round]);
